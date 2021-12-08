9f5ff8ce6f356e3f8cc6a00ed666bedb869ad228
Esad Dursun <3723esad@gmail.com>
2021-12-08 12:08:04 +0100

    update existing products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -125,4 +125,14 @@ class Products with ChangeNotifier {
     // _items.insert(0, newProduct); // adds new product at the start of the list
     notifyListeners();
   }
+
+  void updateProduct(String id, Product newProduct) {
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
+      _items[prodIndex] = newProduct;
+      notifyListeners();
+    } else {
+      print('...');
+    }
+  }
 }
