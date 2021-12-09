318e1957ab48664b3b9d0c5da5e3cb6e94b8fd78
Esad Dursun <3723esad@gmail.com>
2021-12-09 17:06:07 +0100

    async awaits added

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -116,24 +116,23 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> addProduct(Product product) {
+  Future<void> addProduct(Product product) async {
     final url = Uri.https(
         'flutter-shopping-app-f9912-default-rtdb.europe-west1.firebasedatabase.app',
         '/products.json');
-    return http
-        .post(
-      url,
-      body: json.encode(
-        {
-          'title': product.title,
-          'description': product.description,
-          'price': product.price,
-          'imageUrl': product.imageUrl,
-          'isFavourite': product.isFavourite,
-        },
-      ),
-    )
-        .then((response) {
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'title': product.title,
+            'description': product.description,
+            'price': product.price,
+            'imageUrl': product.imageUrl,
+            'isFavourite': product.isFavourite,
+          },
+        ),
+      );
       final newProduct = Product(
         title: product.title,
         description: product.description,
@@ -144,10 +143,10 @@ class Products with ChangeNotifier {
       _items.add(newProduct);
       // _items.insert(0, newProduct); // adds new product at the start of the list
       notifyListeners();
-    }).catchError((error) {
+    } catch (error) {
       print(error);
       throw error;
-    });
+    }
   }
 
   void updateProduct(String id, Product newProduct) {
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -83,7 +83,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
   }
 
-  void _saveForm() {
+  Future<void> _saveForm() async {
     final isValid = _form.currentState.validate();
     if (!isValid) {
       return;
@@ -106,9 +106,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
       );
       Navigator.of(context).pop();
     } else {
-      Provider.of<Products>(context, listen: false)
-          .addProduct(_editedProduct)
-          .catchError(
+      try {
+        await Provider.of<Products>(context, listen: false)
+            .addProduct(_editedProduct);
+      } catch (error) {
         (error) {
           showDialog(
             context: context,
@@ -125,19 +126,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
               ],
             ),
           );
-        },
-      ).then(
-        (_) {
-          setState(
-            () {
-              _isLoading = false;
-            },
-          );
-          Navigator.of(context).pop();
-        },
-      );
+        };
+      } finally {
+        setState(
+          () {
+            _isLoading = false;
+          },
+        );
+        Navigator.of(context).pop();
+      }
     }
-    // Navigator.of(context).pop();
   }
 
   @override
