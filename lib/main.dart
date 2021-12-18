51b0c15b7c093851896d6872f9f1eb06eaf910e4
Esad Dursun <3723esad@gmail.com>
2021-12-18 16:26:08 +0100

    attach orders to users

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProxyProvider<Auth, Orders>(
           update: (ctx, auth, previousOrder) => Orders(
             auth.token,
+            auth.userId,
             previousOrder == null ? [] : previousOrder.orders,
           ),
         )
